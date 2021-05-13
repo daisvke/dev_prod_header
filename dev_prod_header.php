bf3137900eff8671c9e1e2e90e96afd5f9fbe2f4
dtanigaw <daisuketanigawa@live.fr>
2021-05-13 05:27:07 +0200

    Comment error checking

--- dev_prod_header.php
+++ dev_prod_header.php
@@ -24,8 +24,10 @@ if ($devOrProd == 1) { // If development, enter data from your local settings
 // Reach database
 try {
     $db = new PDO('mysql:host='.DB_HOST.'; dbname='.DB_NAME.';charset=utf8', DB_USER, DB_PASSWORD);
+	/* For error checking
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+	*/
 } catch (Exception $e) {
     die('Error : ' . $e->getMessage());
 }
