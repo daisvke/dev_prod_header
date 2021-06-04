eba6b235764e4bdebddbf522d61f4892774d836b
daisvke <65622360+daisvke@users.noreply.github.com>
2021-06-04 06:20:40 +0200

    Update dev_prod_header.php

--- dev_prod_header.php
+++ dev_prod_header.php
@@ -2,7 +2,8 @@
 // Development = 1, production = anything else
 $devOrProd = 1;
 
-if ($devOrProd == 1) { // If development, enter data from your local settings
+if ($devOrProd == 1)
+{ // If development, enter data from your local settings
     // URL from localhost
     define("URL", "http://localhost/mywebsite");
     // PDO request
@@ -10,25 +11,30 @@ if ($devOrProd == 1) { // If development, enter data from your local settings
     define("DB_NAME", "mylocal_db_name");
     define("DB_USER", "root");
     define("DB_PASSWORD", "");
-    // reCAPTCHA
-    define("RECAPTCHA_SUCCESS", "true");
-} else { // If production, enter data from your host settings
+    // Show all PHP errors
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+}
+else
+{ // If production, enter data from your host settings
     define("URL", "https://mywebsite");
     define("DB_HOST", "myhostname");
     define("DB_NAME", "mydbname");
     define("DB_USER", "mydbusername");
     define("DB_PASSWORD", "mydbpassword");
-    define("RECAPTCHA_SUCCESS", "$recaptcha->success == true && $recaptcha->score >= 0.5 && $recaptcha->action == 'contact'");
+    // Hide all PHP errors
+    error_reporting(0);
+    ini_set('display_errors', 0);
 }
 
 // Reach database
 try {
     $db = new PDO('mysql:host='.DB_HOST.'; dbname='.DB_NAME.';charset=utf8', DB_USER, DB_PASSWORD);
-	/* For error checking
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-	*/
+        // For error checking
+        if ($devOrProd == 1) { // If development mode
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        }
 } catch (Exception $e) {
     die('Error : ' . $e->getMessage());
 }
-?>
